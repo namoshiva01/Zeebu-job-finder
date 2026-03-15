dcc87d701d4c715020aed92f60ed372700bf2341
namoshiva01 <91678866+namoshiva01@users.noreply.github.com>
2026-03-16 00:43:25 +0530

    Updated Home.tsx to replace Link component with anchor tag.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Home = () => {
-    return (
-        <div>
-            <section className="relative min-h-[80vh] flex items-center justify-center pt-48 pb-20">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold text-white">Welcome to Zeebu Job Finder</h1>
-                    <p className="mt-4 text-lg text-gray-300">Find your dream job today!</p>
-                    <Link to="/jobs" className="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded">Browse Job Listings</Link>
-                </div>
-            </section>
-        </div>
-    );
-};
-
-export default Home;
+<a href="#" className="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded">Browse Job Listings</a>
