1073c5b9c33f3aa8a87ab79c8702fe6083e99dbe
namoshiva01 <91678866+namoshiva01@users.noreply.github.com>
2026-03-16 00:50:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { useEffect, useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
+import Home from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
