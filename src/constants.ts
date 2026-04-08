42709c7c9e921d6fee3acad225c7e893ffbbb627
namoshiva01 <91678866+namoshiva01@users.noreply.github.com>
2026-04-08 19:53:23 +0530

    feat: Update app download URL
    Updates the PLAY_STORE_URL constant to point to the new APK version (1.1).

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const APP_NAME = "Zeebu";
-export const PLAY_STORE_URL = "https://www.dropbox.com/scl/fi/g6zl2ad9o6ffd9kacd55l/zeebu1.0.apk?rlkey=y6zlamowz84zlnzcl6vgzik0j&st=1m9744uu&dl=1";
+export const PLAY_STORE_URL = "https://www.dropbox.com/scl/fi/mxjl7n3pz984ut9gnzgak/zeebu1.1.apk?rlkey=froe8u7ltqrm04thlut0pm8t8&st=cajr1e3o&dl=1";
